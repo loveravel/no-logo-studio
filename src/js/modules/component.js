3af7a878752816e6a0ff85f8051bd5be8e3a17c6
Sergei Odintsov <loveravel@yandex.ru>
2019-12-10 00:18:42 +0300

    feat: added a component class, which is the base for other classes

---
+++ src/js/modules/component.js
@@ -0,0 +1,43 @@
+import * as tools from '../tools.js';
+
+export default class Component {
+  constructor() {
+    if (new.target === Component) {
+      throw new Error('Can\'t instantiate BaseComponent, only concrete one.');
+    }
+
+    this._element = null;
+  }
+
+  get element() {
+    return this._element;
+  }
+
+  get template() {
+    throw new Error('You have to define template.');
+  }
+
+  render() {
+    this._element = tools.createElement(this.template);
+    this.bind();
+    return this._element;
+  }
+
+  bind() {}
+
+  unbind() {}
+
+  unrender() {
+    this.unbind();
+    this._element.remove();
+    this._element = null;
+  }
+
+  update() {}
+
+  _partialUpdate() {
+    const parentElement = this._element.parentNode;
+    const oldElement = this._element;
+    parentElement.replaceChild(this.render(), oldElement);
+  }
+}
